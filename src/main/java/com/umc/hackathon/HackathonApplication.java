c0cd7f1df2679825a8ee92028bf6c4486f644be9
lsy1307 <lsy1307@inha.edu>
2025-01-12 07:32:56 +0900

    feat: time zone 설정

--- src/main/java/com/umc/hackathon/HackathonApplication.java
+++ src/main/java/com/umc/hackathon/HackathonApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class HackathonApplication {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(HackathonApplication.class, args);
     }
 }
