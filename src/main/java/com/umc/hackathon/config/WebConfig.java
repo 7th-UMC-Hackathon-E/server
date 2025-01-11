b30fb29c63c990681bfd1345eb92c475ab955955
lsy1307 <lsy1307@inha.edu>
2025-01-12 04:45:38 +0900

    feat: CORS config구현

--- src/main/java/com/umc/hackathon/config/WebConfig.java
+++ src/main/java/com/umc/hackathon/config/WebConfig.java
@@ -9,10 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("*") // 허용할 출처
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                .allowedHeaders("*") // 허용할 헤더
-                .allowCredentials(true); // 자격 증명 허용 (예: 쿠키)
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 클라이언트의 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false); // 자격 증명 허용
     }
 }
