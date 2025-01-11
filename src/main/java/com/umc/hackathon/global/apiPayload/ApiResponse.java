7784353c6d6b142928df821ef4e6653c6845e632
[Kkimdoyeon] <[ehdus5176@naver.com]>
2025-01-12 05:44:57 +0900

    Refactor: 안 쓰는 import문 삭제

--- src/main/java/com/umc/hackathon/global/apiPayload/ApiResponse.java
+++ src/main/java/com/umc/hackathon/global/apiPayload/ApiResponse.java
@@ -3,7 +3,6 @@ package com.umc.hackathon.global.apiPayload;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.umc.hackathon.global.apiPayload.code.BaseCode;
 import com.umc.hackathon.global.apiPayload.code.status.SuccessStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
